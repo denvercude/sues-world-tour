c2aa8e770aeff56734e5b6363bda89f3f2f0402a
Denver <cudedenver@gmail.com>
2025-07-08 13:26:29 -0700

    chore(app): initialize project and clean up boilerplate code

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import './globals.css';
+
+export const metadata = {
+  title: "Sue's World Tour",
+  description: "Track Sue the T-Rex as she travels the world.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>{"Sue's World Tour"}</h1>
+      <p>{"Welcome to Sue's World Tour!"}</p>
+    </main>
+  );
+}
