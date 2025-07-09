8ee2441555d18280dff36f8b6a756da36002797a
Denver <cudedenver@gmail.com>
2025-07-09 14:47:37 -0700

    feat(posts): add function to insert new Sue post into Supabase

---
+++ app/lib/addSuePost.ts
@@ -0,0 +1,49 @@
+import { supabase } from './supabaseClient';
+
+// Defines the TypeScript interface, provides a blueprint
+// for what shape the post object should have.
+interface AddSuePostParams {
+    photoUrl: string;
+    location: string;
+    caption: string;
+    secretUsed: string;
+}
+
+// The first part of this function uses the interface to more cleanly
+// destructure the passed in parameters.
+// It does the same thing that this would:
+// function addSuePost(params: AddSuePostParams) {
+// const photoUrl = params.photoUrl;
+// ...
+// }
+export async function addSuePost({
+    photoUrl,
+    location,
+    caption,
+    secretUsed,
+}: AddSuePostParams) {
+    // This part of the function targets the posts table and
+    // inserts a new row with the data from the parameters passed in.
+    // Supabase insert() method expects an array, allowing multiple row
+    // insertions at once. In our case, we are only inserting
+    // 1 row.
+    const { data, error } = await supabase.from('posts').insert([
+        {
+            photo_url: photoUrl,
+            location,
+            caption,
+            secret_used: secretUsed,
+        },
+    ]);
+
+    // The function above immediatley pulls out the response objects from
+    // Supabase into error and data variable, so they are ready to use.
+
+    // Handle errors in database insertion
+    if(error) {
+        console.error('Error inserting post:', error.message);
+        throw new Error(`Failed to add post: ${error.message}`);
+    }
+
+    return data;
+}
