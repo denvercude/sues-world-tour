e0cd04dfda12b0a0e3ab8682b446817964351122
Denver <cudedenver@gmail.com>
2025-07-10 15:18:44 -0700

    fix(upload): add cursor pointer on hover over file upload input

--- components/retroui/InputWithLabel.tsx
+++ components/retroui/InputWithLabel.tsx
@@ -12,7 +12,12 @@ export default function InputStyleWithLabel(props: InputWithLabelProps) {
   return (
     <div className="w-full grid gap-1.5 p-2">
       <Label htmlFor={props.id}>{props.label}</Label>
-      <Input type={props.type} id={props.id} placeholder={props.placeholder} />
+      <Input 
+        type={props.type} 
+        id={props.id} 
+        placeholder={props.placeholder}
+        className={props.type === "file" ? "cursor-pointer" : ""}
+      />
     </div>
   );
 }
