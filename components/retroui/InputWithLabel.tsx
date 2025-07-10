419b9b3a459d6895a6d695ad0314d58fe6b0d670
Denver <cudedenver@gmail.com>
2025-07-10 14:17:57 -0700

    feat(upload): create initial ui for posting a photo

---
+++ components/retroui/InputWithLabel.tsx
@@ -0,0 +1,18 @@
+import { Input } from "@/components/retroui/Input";
+import { Label } from "@/components/retroui/Label";
+
+interface InputWithLabelProps {
+  label: string;
+  type: string;
+  id: string;
+  placeholder: string;
+}
+
+export default function InputStyleWithLabel(props: InputWithLabelProps) {
+  return (
+    <div className="w-full grid gap-1.5 p-2">
+      <Label htmlFor="file">{props.label}</Label>
+      <Input type={props.type} id={props.id} placeholder={props.placeholder} />
+    </div>
+  );
+}
