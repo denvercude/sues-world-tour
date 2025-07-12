a00d7549ba1a16ebe00c045be5d18741f5acd598
Denver <cudedenver@gmail.com>
2025-07-12 08:55:45 -0700

    refactor(upload-nav): restyle the upload form and add basic nav between landing page, gallery, and upload page

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,8 +1,12 @@
+import UploadLinkPolaroid from "@/components/UploadLinkPolaroid";
+
 export default function GalleryPage() {
     return (
         <main>
-            <h1>Gallery Page</h1>
-            <p>{"This is the gallery page for Sue's World Tour."}</p>
+            <h1 className="flex justify-center text-7xl">Gallery Page</h1>
+            <div className="h-screen flex justify-center items-center">
+                <UploadLinkPolaroid />
+            </div>
         </main>
     );
 } 
---
+++ components/UploadLinkPolaroid.tsx
@@ -0,0 +1,18 @@
+import { Card } from "@/components/retroui/Card";
+import { Button } from "@/components/retroui/Button"
+import Link from "next/link";
+
+export default function UploadLinkPolaroid() {
+    return (
+        <Card className="w-[300px] shadow-none hover:shadow-none">
+            <Card.Content>
+                <div className="w-full aspect-square border-dashed border-2"></div>
+            </Card.Content>
+            <Card.Content className="flex-col justify-content items-center">
+                <Link href="/upload">
+                <Button className="m-1">Add a photo</Button>
+                </Link>
+            </Card.Content>
+        </Card>
+    );
+}
