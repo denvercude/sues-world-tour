4980c2d14ef1bbc9d2da1c348d4a813e701df413
Denver <cudedenver@gmail.com>
2025-07-23 15:36:23 -0700

    feat(landing-page): add brutalist styling to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,27 @@
 "use client";
-import Link from "next/link";
-import { Button } from "@/components/retroui/Button";
+import TitleBar from "@/components/TitleBar";
+import { Text } from "@/components/retroui/Text";
 
 export default function Home() {
   return (
     <main>
-      <h1 className="flex justify-center text-7xl">{"Sue's World Tour Test"}</h1>
-      <div className="min-h-[100vh] flex justify-center items-center">
-        <Link href="/gallery">
-          <Button>Get Started</Button>
-        </Link>
+      <div className="h-screen w-screen">
+        <div className="h-full w-full border-8 bg-[#d9bcb4]">
+          <TitleBar />
+          <div className="h-3/4 w-full flex justify-center items-center p-10">
+            <div className="w-1/3 h-full m-20 bg-white flex flex-col pt-5 px-5">
+              <div className="w-full h-4/6 border-2 bg-black" />
+              <Text className="text-6xl font-bold pt-5">ABOUT</Text>
+            </div>
+            <div className="w-1/3 h-full m-20 bg-white flex flex-row py-5 px-5">
+              <div className="w-4/6 h-full border-2 bg-black" />
+            </div>
+            <div className="w-1/3 h-full m-20 bg-white flex flex-col pt-5 px-5">
+              <div className="w-full h-4/6 border-2 bg-black" />
+              <Text className="text-6xl font-bold pt-5">SOMETHING</Text>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
---
+++ components/TitleBar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text } from "@/components/retroui/Text";
+import Link from "next/link";
+
+export default function TitleBar() {
+    return (
+        <div className="flex justify-center items-center h-40 w-full relative">
+            <div className="w-full h-full flex flex-col">
+                <div className="bg-black h-1/2 flex justify-end items-center gap-12 pr-30">
+                    <Link href="./gallery">
+                        <Text className="text-[#e23123] font-bold">Gallery</Text>
+                    </Link>
+                    <Link href="./upload">
+                        <Text className="text-[#f0b83e] font-bold">Upload</Text>
+                    </Link>
+                </div>
+                <div className="h-1/2"></div>
+            </div>
+            <div className="border-2 border-black bg-white px-8 py-2 font-bold shadow-[6px_6px_0px_0px_rgba(34,34,34,1)] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 whitespace-nowrap">
+                <Text className="text-2xl">SUE'S WORLD TOUR</Text>
+            </div>
+        </div>
+    );
+}
